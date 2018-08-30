007c5867e45233b90a72465fe866a4833cb7a919
Martin Rossil <martinrossil@gmail.com>
2018-08-30 12:18:38 +0200

    Added website Config.

---
+++ src/dk/website/app/Config.js
@@ -0,0 +1,8 @@
+export default class Config
+{
+    static get FIRE_BASE_DB_BASE_URL(){return "https://avaapp-4786c.firebaseio.com/";};
+    static get FIRE_BASE_STORAGE_BASE_URL(){return "https://firebasestorage.googleapis.com/v0/b/avaapp-4786c.appspot.com/o/";};
+    static get APP_BACKGROUND_COLOR(){return "#242f45";};
+    static get PRIMARY_COLOR(){return "#01867c";};
+    static get SECONDARY_COLOR(){return "#353d50";};
+}
