685f4b041edad13f4c423cc1d12285148a025fc7
Martin Rossil <martinrossil@gmail.com>
2018-08-30 12:18:24 +0200

    Added website ClickTargetUtil.

---
+++ src/dk/website/ClickTargetUtil.js
@@ -0,0 +1,53 @@
+import BaseItemRenderer from "../ava/components/itemrenderers/BaseItemRenderer.js";
+import IconButton from "../ava/components/buttons/IconButton.js";
+export default class ClickTargetUtil
+{
+    constructor()
+    {
+    }
+    static getATagFromTarget( target )
+    {
+        if( target instanceof HTMLAnchorElement )
+        {
+            return target;
+        }
+        else if( target instanceof HTMLDocument )
+        {
+            return null;
+        }
+        else
+        {
+            return this.getATagFromTarget( target.parentNode );
+        }
+    }
+    static getIconButtonFromTarget( target )
+    {
+        if( target instanceof IconButton )
+        {
+            return target;
+        }
+        else if( target instanceof HTMLDocument )
+        {
+            return null;
+        }
+        else
+        {
+            return this.getIconButtonFromTarget( target.parentNode );
+        }
+    }
+    static getItemRendererFromTarget( target )
+    {
+        if( target instanceof BaseItemRenderer )
+        {
+            return target;
+        }
+        else if( target instanceof HTMLDocument )
+        {
+            return null;
+        }
+        else
+        {
+            return this.getItemRendererFromTarget( target.parentNode );
+        }
+    }
+}
