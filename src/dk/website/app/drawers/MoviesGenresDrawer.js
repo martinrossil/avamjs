0910867671c3515ca70e4486b65473061de28e15
Martin Rossil <martinrossil@gmail.com>
2018-08-31 12:20:19 +0200

    Removed Model.

--- src/dk/website/app/drawers/MoviesGenresDrawer.js
+++ src/dk/website/app/drawers/MoviesGenresDrawer.js
@@ -4,7 +4,6 @@ import GenreItemRenderer from "../itemrenderers/GenreItemRenderer.js";
 import AnchorLayoutData from "../../../ava/layouts/data/AnchorLayoutData.js";
 import ScrollPolicy from "../../../ava/constants/ScrollPolicy.js";
 import ListElement from "../../../ava/components/lists/ListElement.js";
-import Model from "../../model/Model.js";
 export default class MoviesGenresDrawer extends BaseDrawer
 {
     constructor()
@@ -25,11 +24,11 @@ export default class MoviesGenresDrawer extends BaseDrawer
         if( !this._genresList )
         {
             this._genresList = new ListElement();
+            this._genresList.uid = "movieGenresList";
             this._genresList.horizontalScrollPolicy = ScrollPolicy.OFF;
             this._genresList.layout = this.verticalLayout;
             this._genresList.layoutData = new AnchorLayoutData( 16, 56, 16, 16 );
             this._genresList.itemRenderType = GenreItemRenderer;
-            this._genresList.dataProvider = Model.moviesGenresCollection;
         }
         return this._genresList;
     }
--- src/dk/website/app/drawers/TrailersGenresDrawer.js
+++ src/dk/website/app/drawers/TrailersGenresDrawer.js
@@ -3,11 +3,7 @@ import VerticalLayout from "../../../ava/layouts/VerticalLayout.js";
 import AnchorLayoutData from "../../../ava/layouts/data/AnchorLayoutData.js";
 import ListElement from "../../../ava/components/lists/ListElement.js";
 import ScrollPolicy from "../../../ava/constants/ScrollPolicy.js";
-import Model from "../../model/Model.js";
 import GenreItemRenderer from "../itemrenderers/GenreItemRenderer.js";
-import ArrayCollection from "../../../ava/data/ArrayCollection.js";
-import JSONLoader from "../../../ava/loaders/JSONLoader.js";
-import EventTypes from "../../../ava/constants/EventTypes.js";
 export default class TrailersGenresDrawer extends BaseDrawer
 {
     constructor()
@@ -28,10 +24,10 @@ export default class TrailersGenresDrawer extends BaseDrawer
         if( !this._genresList )
         {
             this._genresList = new ListElement();
+            this._genresList.uid = "trailersGenresList";
             this._genresList.horizontalScrollPolicy = ScrollPolicy.OFF;
             this._genresList.layout = this.verticalLayout;
             this._genresList.layoutData = new AnchorLayoutData( 16, 56, 16, 16 );
-            this._genresList.dataProvider = Model.trailersGenresCollection;
             this._genresList.itemRenderType = GenreItemRenderer;
         }
         return this._genresList;
