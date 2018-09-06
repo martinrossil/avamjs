fef9934ee761080a7dff860bef15a35ec29f3401
Martin Rossil <martinrossil@gmail.com>
2018-09-06 11:08:33 +0200

    Removed title property and firstShow().

--- src/dk/website/app/drawers/BaseDrawer.js
+++ src/dk/website/app/drawers/BaseDrawer.js
@@ -5,7 +5,6 @@ import AnchorLayout from "../../../ava/layouts/AnchorLayout.js";
 import IconButton from "../../../ava/components/buttons/IconButton.js";
 import IconNames from "../../../ava/constants/IconNames.js";
 import AnchorLayoutData from "../../../ava/layouts/data/AnchorLayoutData.js";
-import TextElement from "../../../ava/components/text/TextElement.js";
 import Theme from "../../../ava/styles/Theme.js";
 import EventTypes from "../../../ava/constants/EventTypes.js";
 export default class BaseDrawer extends LayoutContainer
@@ -31,19 +30,11 @@ export default class BaseDrawer extends LayoutContainer
             this.isVisible = true;
         }
         this.x = this.isShown ? window.innerWidth - this.width : window.innerWidth;
-        if( !this.hasBeenShown )
-        {
-            this.hasBeenShown = true;
-            this.firstShow();
-        }
-    }
-    firstShow()
-    {
-        // override
     }
     initialize()
     {
         super.initialize();
+        this.backgroundColor = Theme.PRIMARY_COLOR_DARK;
         this.layout = new AnchorLayout();
         this.resizeAndPosition = this.resizeAndPosition.bind( this );
         this.setSize( 4 * 56, window.innerHeight );
@@ -55,21 +46,7 @@ export default class BaseDrawer extends LayoutContainer
         this.listen( EventTypes.PROPERTY_ANIMATION_ENDED, this.propertyAnimationEnded.bind( this ) );
         this.animatedProperties = [ new AnimatedProperty( "x", 225, "ease-in" ) ];
         window.addEventListener( "resize", this.resizeAndPosition );
-        this.addElement( this.titleTextElement );
         this.addElement( this.closeIconButton ); 
-    }
-    get titleTextElement()
-    {
-        if( !this._titleTextElement )
-        {
-            this._titleTextElement = new TextElement();
-            this._titleTextElement.layoutData = new AnchorLayoutData( 16, NaN, 56 );
-            this._titleTextElement.setPosition( 16, 13 );
-            this._titleTextElement.textColor = Theme.PRIMARY_TEXT_COLOR;
-            this._titleTextElement.fontSize = 20;
-            this._titleTextElement.wordWrap = false;
-        }
-        return this._titleTextElement;
     }
     get closeIconButton()
     {
@@ -97,18 +74,6 @@ export default class BaseDrawer extends LayoutContainer
     get isShown()
     {
         return this._isShown;
-    }
-    set title( value )
-    {
-        if( this._title !== value )
-        {
-            this._title = value;
-            this._titleTextElement.text = value;
-        }
-    }
-    get title()
-    {
-        return this._title;
     }
     set closeHref( value )
     {
