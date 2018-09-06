0d3577813d387afaf58e113e5b457fbf29264506
Martin Rossil <martinrossil@gmail.com>
2018-09-06 11:07:56 +0200

    Refactor.

--- src/dk/website/app/drawers/TrailersDrawer.js
+++ src/dk/website/app/drawers/TrailersDrawer.js
@@ -3,14 +3,11 @@ import VerticalLayout from "../../../ava/layouts/VerticalLayout.js";
 import AnchorLayoutData from "../../../ava/layouts/data/AnchorLayoutData.js";
 import ListElement from "../../../ava/components/lists/ListElement.js";
 import ScrollPolicy from "../../../ava/constants/ScrollPolicy.js";
-import GenreItemRenderer from "../itemrenderers/GenreItemRenderer.js";
-import FilterTypeItemRenderer from "../itemrenderers/FilterTypeItemRenderer.js";
 import ArrayCollection from "../../../ava/data/ArrayCollection.js";
 import IconNames from "../../../ava/constants/IconNames.js";
 import DisplayElement from "../../../ava/components/display/DisplayElement.js";
 import Theme from "../../../ava/styles/Theme.js";
-import Config from "../Config.js";
-import CountryItemRenderer from "../itemrenderers/CountryItemRenderer.js";
+import LinkItemRenderer from "../itemrenderers/base/LinkItemRenderer.js";
 export default class TrailersDrawer extends BaseDrawer
 {
     constructor()
@@ -30,9 +27,8 @@ export default class TrailersDrawer extends BaseDrawer
     }
     createChildren()
     {
-        this.addElement( this.filterTypesList );
-        this.addElement( this.genresList );
-        this.addElement( this.countriesList );
+        this.addElement( this.trailersFilterList );
+        this.addElement( this.trailersDrawerList );
     }
     get topBlock()
     {
@@ -45,62 +41,45 @@ export default class TrailersDrawer extends BaseDrawer
         }
         return this._topBlock;
     }
-    get filterTypesList()
+    get trailersFilterList()
     {
-        if( !this._filterTypesList )
+        if( !this._trailersFilterList )
         {
-            this._filterTypesList = new ListElement();
-            this._filterTypesList.height = 96;
-            this._filterTypesList.uid = "trailersFilterTypesList";
-            this._filterTypesList.horizontalScrollPolicy = ScrollPolicy.OFF;
-            this._filterTypesList.layout = new VerticalLayout();
-            this._filterTypesList.layoutData = new AnchorLayoutData( 8, 52, 8 );
-            this._filterTypesList.itemRenderType = FilterTypeItemRenderer;
-            this._filterTypesList.dataProvider = this.filterTypesListDataProvider;
-            this._filterTypesList.selectedIndex = 0;
+            this._trailersFilterList = new ListElement();
+            this._trailersFilterList.height = 96;
+            this._trailersFilterList.uid = "trailersFilterList";
+            this._trailersFilterList.horizontalScrollPolicy = ScrollPolicy.OFF;
+            this._trailersFilterList.layout = new VerticalLayout();
+            this._trailersFilterList.layoutData = new AnchorLayoutData( 8, 52, 8 );
+            this._trailersFilterList.itemRenderType = LinkItemRenderer;
+            this._trailersFilterList.dataProvider = this.trailersFilterListDataProvider;
+            this._trailersFilterList.selectedIndex = 0;
         }
-        return this._filterTypesList;
+        return this._trailersFilterList;
     }
-    get filterTypesListDataProvider()
+    get trailersFilterListDataProvider()
     {
-        if( !this._filterTypesListDataProvider )
+        if( !this._trailersFilterListDataProvider )
         {
-            let filters = [ { icon : IconNames.MOVIE_FILTER, label : "Genrer", href : "/trailers/genrer" }, 
-                            { icon : IconNames.PUBLIC, label : "Lande", href : "/trailers/lande" } 
+            let filters = [ { icon : IconNames.MOVIE_FILTER, l : "Genrer", h : "/trailers/filter/genrer" }, 
+                            { icon : IconNames.PUBLIC, l : "Lande", h : "/trailers/filter/lande" } 
                           ];    
-            this._filterTypesListDataProvider = new ArrayCollection( filters );
+            this._trailersFilterListDataProvider = new ArrayCollection( filters );
         }
-        return this._filterTypesListDataProvider;
+        return this._trailersFilterListDataProvider;
     }
-    get genresList()
+    get trailersDrawerList()
     {
-        if( !this._genresList )
+        if( !this._trailersDrawerList )
         {
-            this._genresList = new ListElement();
-            this._genresList.uid = "trailersGenresList";
-            this._genresList.horizontalScrollPolicy = ScrollPolicy.OFF;
-            this._genresList.layout = new VerticalLayout();
-            this._genresList.layoutData = new AnchorLayoutData( 8, 60 + 96, 8, 16 );
-            this._genresList.itemRenderType = GenreItemRenderer;
-            this._genresList.selectedIndex = 0;
+            this._trailersDrawerList = new ListElement();
+            this._trailersDrawerList.uid = "trailersDrawerList";
+            this._trailersDrawerList.horizontalScrollPolicy = ScrollPolicy.OFF;
+            this._trailersDrawerList.layout = new VerticalLayout();
+            this._trailersDrawerList.layoutData = new AnchorLayoutData( 8, 60 + 96, 8, 16 );
+            this._trailersDrawerList.itemRenderType = LinkItemRenderer;
         }
-        return this._genresList;
-    }
-    get countriesList()
-    {
-        if( !this._countriesList )
-        {
-            this._countriesList = new ListElement();
-            this._countriesList.isVisible = false;
-            this._countriesList.isInteractive = false;
-            this._countriesList.uid = "trailersCountriesList";
-            this._countriesList.horizontalScrollPolicy = ScrollPolicy.OFF;
-            this._countriesList.layout = new VerticalLayout();
-            this._countriesList.layoutData = new AnchorLayoutData( 8, 60 + 96, 8, 16 );
-            this._countriesList.itemRenderType = CountryItemRenderer;
-            this._countriesList.selectedIndex = 0;
-        }
-        return this._countriesList;
+        return this._trailersDrawerList;
     }
 }
 customElements.define("trailers-drawer", TrailersDrawer ); 
