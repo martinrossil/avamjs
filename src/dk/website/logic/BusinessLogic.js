2b9de7944ff27a422e9a498927e75ec711ace074
Martin Rossil <martinrossil@gmail.com>
2018-09-11 11:13:42 +0200

    New Logic Classes.

--- src/dk/website/logic/BusinessLogic.js
+++ src/dk/website/logic/BusinessLogic.js
@@ -4,15 +4,29 @@ import TrailerPlaybackLogic from "./TrailerPlaybackLogic.js";
 import ShowMovieInfoLogic from "./ShowMovieInfoLogic.js";
 import FilterByGenreLogic from "./FilterByGenreLogic.js";
 import DataModelLogic from "./DataModelLogic.js";
+import DataLogic from "./DataLogic.js";
+import ShowHideUILogic from "./ShowHideUILogic.js";
+import HistoryLogic from "./HistoryLogic.js";
+import ShowHideDrawersAndMenusLogic from "./ShowHideDrawersAndMenusLogic.js";
+import TopNavigationLogic from "./TopNavigationLogic.js";
+import DrawersDataLoadLogic from "./DrawersDataLoadLogic.js";
+import ListsDataLoadLogic from "./ListsDataLoadLogic.js";
 export default class BusinessLogic
 {
     constructor()
     {
-        new AppBarsVisibilityLogic();
-        new TopLevelNavigationLogic();
-        new TrailerPlaybackLogic();
-        new ShowMovieInfoLogic();
-        new FilterByGenreLogic();
-        new DataModelLogic();
+        //new AppBarsVisibilityLogic();
+        //new TopLevelNavigationLogic();
+        //new TrailerPlaybackLogic();
+        //new ShowMovieInfoLogic();
+        //new FilterByGenreLogic();
+        //new DataModelLogic();
+        //new ShowHideUILogic();
+        //new DataLogic();
+        //new HistoryLogic();
+        new TopNavigationLogic();
+        new ListsDataLoadLogic();
+        new ShowHideDrawersAndMenusLogic();
+        new DrawersDataLoadLogic();
     }
 }
