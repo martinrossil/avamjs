49793fd552ac7f5ab9238cace9429616c7d2c1dd
Martin Rossil <martinrossil@gmail.com>
2018-09-11 10:19:09 +0200

    Refactored.

--- src/dk/website/app/drawers/ActorsDrawer.js
+++ src/dk/website/app/drawers/ActorsDrawer.js
@@ -2,13 +2,23 @@ import BaseDrawer from "./BaseDrawer.js";
 import ListElement from "../../../ava/components/lists/ListElement.js";
 import ScrollPolicy from "../../../ava/constants/ScrollPolicy.js";
 import AnchorLayoutData from "../../../ava/layouts/data/AnchorLayoutData.js";
-import CountryItemRenderer from "../itemrenderers/CountryItemRenderer.js";
 import VerticalLayout from "../../../ava/layouts/VerticalLayout.js";
+import DisplayElement from "../../../ava/components/display/DisplayElement.js";
+import Theme from "../../../ava/styles/Theme.js";
+import IconButton from "../../../ava/components/buttons/IconButton.js";
+import IconNames from "../../../ava/constants/IconNames.js";
+import LinkItemRenderer from "../itemrenderers/base/LinkItemRenderer.js";
+import ArrayCollection from "../../../ava/data/ArrayCollection.js";
 export default class ActorsDrawer extends BaseDrawer
 {
     constructor()
     {
         super();
+    }
+    sizeChanged( w, h )
+    {
+        super.sizeChanged( w, h );
+        this.topBlock.width = w;
     }
     initialize()
     {
@@ -17,28 +27,71 @@ export default class ActorsDrawer extends BaseDrawer
     }
     createChildren()
     {
-        this.addElement( this.countriesList );
+        this.addElement( this.actorDrawersList );
+        this.addElement( this.topBlock );
+        this.addElement( this.closeIconButton );
+        this.addElement( this.filterList );
     }
-    get countriesList()
+    get topBlock()
     {
-        if( !this._countriesList )
+        if( !this._topBlock )
         {
-            this._countriesList = new ListElement();
-            this._countriesList.uid = "actorsCountriesList";
-            this._countriesList.horizontalScrollPolicy = ScrollPolicy.OFF;
-            this._countriesList.layout = this.verticalLayout;
-            this._countriesList.layoutData = new AnchorLayoutData( 16, 56, 16, 16 );
-            this._countriesList.itemRenderType = CountryItemRenderer;
+            this._topBlock = new DisplayElement();
+            this._topBlock.backgroundColor = Theme.PRIMARY_COLOR_DARK;
+            this._topBlock.z = 2;
+            this._topBlock.height = 52 + 48;
         }
-        return this._countriesList;
+        return this._topBlock;
     }
-    get verticalLayout()
+    get closeIconButton()
     {
-        if( !this._verticalLayout )
+        if( !this._closeIconButton )
         {
-            this._verticalLayout = new VerticalLayout();
+            this._closeIconButton = new IconButton();
+            this._closeIconButton.uid = "actorsDrawerCloseButton";
+            this._closeIconButton.iconName = IconNames.ARROW_FORWARD;
+            this._closeIconButton.layoutData = new AnchorLayoutData( NaN, 4, 4 ); 
         }
-        return this._verticalLayout;
+        return this._closeIconButton;
+    }
+    get filterList()
+    {
+        if( !this._filterList )
+        {
+            this._filterList = new ListElement();
+            this._filterList.height = 48;
+            this._filterList.uid = "actorsFilterList";
+            this._filterList.horizontalScrollPolicy = ScrollPolicy.OFF;
+            this._filterList.layout = new VerticalLayout();
+            this._filterList.layoutData = new AnchorLayoutData( 8, 52, 8 );
+            this._filterList.itemRenderType = LinkItemRenderer;
+            this._filterList.dataProvider = this.filterListDataProvider;
+            this._filterList.selectedIndex = 0;
+        }
+        return this._filterList;
+    }
+    get filterListDataProvider()
+    {
+        if( !this._filterListDataProvider )
+        {
+            let filters = [ { icon : IconNames.PUBLIC, l : "Lande", h : "/skuespillere/lande" } ];    
+            this._filterListDataProvider = new ArrayCollection( filters );
+        }
+        return this._filterListDataProvider;
+    }
+    get actorDrawersList()
+    {
+        if( !this._actorDrawersList )
+        {
+            this._actorDrawersList = new ListElement();
+            this._actorDrawersList.uid = "actorsDrawerList";
+            this._actorDrawersList.horizontalScrollPolicy = ScrollPolicy.OFF;
+            this._actorDrawersList.layout = new VerticalLayout();
+            this._actorDrawersList.layoutData = new AnchorLayoutData( 8, 100, 8, 16 );
+            this._actorDrawersList.itemRenderType = LinkItemRenderer;
+            this._actorDrawersList.selectedIndex = 0;
+        }
+        return this._actorDrawersList;
     }
 }
 customElements.define("actors-drawer", ActorsDrawer ); 
