be8b5a9ae48002d17c6903ceb34cd92ef8a5a3a7
Martin Rossil <martinrossil@gmail.com>
2018-09-11 10:19:59 +0200

    Created AppColors, might remove.

---
+++ src/dk/website/app/AppColors.js
@@ -0,0 +1,12 @@
+export default class AppColors
+{
+    static get RED(){return "#dc471f";};
+    static get ORANGE(){return "#fdb813";};
+    static get LIGHT_ORANGE(){return "#ffd65a";};
+    static get BLACK(){return "#140b0c";};
+    static get WHITE(){return "#fffddc";};
+    static get GREEN(){return "#8ab580";};
+    static get LIGHT_GREEN(){return "#b9d892";};
+    static get BLUE(){return "#577e7b";};
+    static get LIGHT_BLUE(){return "#7db1be";};
+}
