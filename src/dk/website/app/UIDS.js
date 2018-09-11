83122967c01fbd9775e86beca89450764867b258
Martin Rossil <martinrossil@gmail.com>
2018-09-11 10:20:21 +0200

    Created UIDs, preventing mistyped uids.

---
+++ src/dk/website/app/UIDS.js
@@ -0,0 +1,10 @@
+export default class UIDS
+{
+    static get APP(){return "app";};
+    static get OVERLAY(){return "overlay";};
+    static get TRAILERS_DRAWER(){return "trailersDrawer";};
+    static get TRAILERS_DRAWER_ICON_BUTTON(){return "trailersDrawerIconButton";};
+    static get TRAILERS_DRAWER_CLOSE_BUTTON(){return "trailersDrawerCloseButton";};
+    
+    
+}
