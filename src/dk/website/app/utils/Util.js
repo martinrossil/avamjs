32e0911c0f92c34dc4daf6fdf54bc8cb17d48cd6
Martin Rossil <martinrossil@gmail.com>
2018-09-14 21:58:03 +0200

    Added more path methods.

--- src/dk/website/app/utils/Util.js
+++ src/dk/website/app/utils/Util.js
@@ -1,7 +1,93 @@
+import Paths from "../consts/Paths.js";
 export default class Util
 {
     constructor()
     {
+    }
+    static getTopIndex( top )
+    {
+        let index = 0;
+        if( top === "" || top === "trailers" )
+        {
+            index = 0;
+        }
+        else if( top === "film" )
+        {
+            index = 1;
+        }
+        else if( top === "skuespillere" )
+        {
+            index = 2;
+        }
+        return index;
+    }
+    static getMoviesFilterListIndexFromPath( path )
+    {
+        if( path.indexOf( Paths.MOVIES_GENRER ) != -1 )
+        {
+            return 0;
+        }
+        else if( path.indexOf( Paths.MOVIES_COUNTRIES ) != -1 )
+        {
+            return 1;
+        }
+        return 0;
+    }
+    static getTrailersFilterListIndexFromPath( path )
+    {
+        if( path.indexOf( Paths.TRAILERS_GENRER ) != -1 )
+        {
+            return 0;
+        }
+        else if( path.indexOf( Paths.TRAILERS_COUNTRIES ) != -1 )
+        {
+            return 1;
+        }
+        return 0;
+    }
+    static getTopNavigationIndexFromPath( path )
+    {
+        if( path === Paths.ROOT )
+        {
+            return 0;
+        }
+        else if( path.indexOf( Paths.TRAILERS ) !== -1  )
+        {
+            return 0;
+        }
+        else if( path.indexOf( Paths.MOVIES ) !== -1 )
+        {
+            return 1;
+        }
+        else if( path.indexOf( Paths.ACTORS ) !== -1 )
+        {
+            return 2;
+        }
+        return 0;
+    }
+    static getTitleFromPath( path )
+    {
+        let pathArray = path.split( "/" );
+            pathArray.shift();
+        let title = "";    
+        let frag;
+        let first;
+        let i = 0;
+        for( ; i < pathArray.length; i++ )
+        {
+            frag = pathArray[ i ];
+            first = frag.charAt( 0 ).toUpperCase();
+            title += first + frag.substr( 1 );
+            if( i < ( pathArray.length - 1 ) )
+            {
+                title += " / ";
+            }
+        }
+        if( !title )
+        {
+            title = "Trailers";
+        }
+        return title;
     }
     static getImageSize( w )
     {
