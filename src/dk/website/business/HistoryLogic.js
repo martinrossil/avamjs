2232e7f0a93f7d456c3060220633f267e6e738f9
Martin Rossil <martinrossil@gmail.com>
2018-09-21 11:18:42 +0200

    Refactored.

--- src/dk/website/business/HistoryLogic.js
+++ src/dk/website/business/HistoryLogic.js
@@ -1,44 +1,17 @@
-import Logic from "../../ava/logic/Logic.js";
-import Events from "../app/consts/Events.js";
-import ClickTargetUtil from "../app/utils/ClickTargetUtil.js";
 import Util from "../app/utils/Util.js";
-import UIDS from "../app/consts/UIDS.js";
-import EventTypes from "../../ava/constants/EventTypes.js";
-import Paths from "../app/consts/Paths.js";
-export default class HistoryLogic extends Logic
+import BasePathLogic from "./base/BasePathLogic.js";
+export default class HistoryLogic extends BasePathLogic
 {
     constructor()
     {
         super();
-        this.listen( UIDS.APP, EventTypes.APPLICATION_LOAD_COMPLETE, this.applicationLoadComplete.bind( this ) );
-        document.addEventListener( Events.CLICK, this.clicked.bind( this ) );
     }
-    applicationLoadComplete()
+    pathChanged( path )
     {
-        let path = window.location.pathname;
-        if( path === Paths.ROOT )
+        if( path !== window.location.pathname )
         {
-            path = Paths.TRAILERS;
             history.pushState( null, null, path );
-            this.setDocumentTitleFromPath( path );
         }
-    }
-    clicked( e )
-    {
-        let aTag = ClickTargetUtil.getATagFromTarget( e.target );
-        if( aTag )
-        {
-            e.preventDefault();
-            let path = aTag.pathname;
-            if( path !== window.location.pathname )
-            {
-                history.pushState( null, null, path );
-                this.setDocumentTitleFromPath( path );
-            }
-        }
-    }
-    setDocumentTitleFromPath( path )
-    {
         document.title = Util.getTitleFromPath( path );
     }
 }
