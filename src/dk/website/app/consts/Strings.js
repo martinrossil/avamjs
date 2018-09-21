16261beade68e9dbd256b320539ba5c5c19e59c1
Martin Rossil <martinrossil@gmail.com>
2018-09-21 11:21:18 +0200

    Added Strings.

---
+++ src/dk/website/app/consts/Strings.js
@@ -0,0 +1,9 @@
+export default class Strings
+{
+    static get OPEN_FILTER_MENU(){return "Åbn Filter Menu";};
+    static get CLOSE_FILTER_MENU(){return "Luk Filter Menu";};
+    static get CLOSE(){return "Luk";};
+    static get OPEN_SORT_MENU(){return "Åbn Sorterings Menu";};
+    static get GENRES(){return "Genrer";};
+    static get COUNTRIES(){return "Lande";};
+}
