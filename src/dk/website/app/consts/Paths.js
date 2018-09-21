2dbf505633fe71f60e7902b7b0ebf2a69706d30d
Martin Rossil <martinrossil@gmail.com>
2018-09-21 11:13:45 +0200

    Refactored.

--- src/dk/website/app/consts/Paths.js
+++ src/dk/website/app/consts/Paths.js
@@ -2,16 +2,17 @@ export default class Paths
 {
     static get ROOT(){              return "/";};
     static get TRAILERS(){          return "/trailers";};
-    static get TRAILERS_GENRER(){   return "/trailers/genrer";};
+    static get TRAILERS_GENRES(){   return "/trailers/genrer";};
     static get TRAILERS_COUNTRIES(){return "/trailers/lande";};
+
     static get MOVIES(){            return "/film";};
-    static get MOVIES_GENRER(){     return "/film/genrer";};
+    static get MOVIES_GENRES(){     return "/film/genrer";};
     static get MOVIES_COUNTRIES(){  return "/film/lande";};
+
     static get ACTORS(){            return "/skuespillere";};
     static get ACTORS_COUNTRIES(){  return "/skuespillere/lande";};
-    static get GENRER(){            return "/genrer";};
+
+    static get GENRES(){            return "/genrer";};
     static get COUNTRIES(){         return "/lande";};
-    static get TRAILERS_FILTER(){   return "/trailers/filter";};
-    static get MOVIES_FILTER(){     return "/film/filter";};
-    static get ACTORS_FILTER(){     return "/skuespillere/filter";};
+
 }
