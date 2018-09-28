a8e3c8330f3d832fbb9089e6d048b31304335004
Martin Rossil <martinrossil@gmail.com>
2018-09-28 10:01:14 +0200

    Cleaned.

--- src/dk/website/app/bars/AvaAppBar.js
+++ src/dk/website/app/bars/AvaAppBar.js
@@ -5,8 +5,6 @@ import Theme from "../../../ava/styles/Theme.js";
 import IconButton from "../../../ava/components/buttons/IconButton.js";
 import IconNames from "../../../ava/constants/IconNames.js";
 import UIDS from "../consts/UIDS.js";
-import LinkIconButton from "../../../ava/components/buttons/LinkIconButton.js";
-import Paths from "../consts/Paths.js";
 import Strings from "../consts/Strings.js";
 export default class AvaAppBar extends AppBar
 {
@@ -22,24 +20,8 @@ export default class AvaAppBar extends AppBar
     createChildren()
     {
         this.addElement( this.titleTextElement );
-        this.addElement( this.openSortMenuIconButton );
+        //this.addElement( this.openSortButton );
         this.addElement( this.openFilterButton );
-        /*this.addElement( this.openTrailersFilterButton );
-        this.addElement( this.openMoviesFilterButton );
-        this.addElement( this.openActorsFilterButton );*/
-    }
-    get openFilterButton()
-    {
-        if( !this._openFilterButton )
-        {
-            this._openFilterButton = new LinkIconButton();
-            //this._openFilterButton.href = Paths.TRAILERS_GENRER;
-            this._openFilterButton.ariaLabel = Strings.OPEN_FILTER_MENU;
-            this._openFilterButton.uid = UIDS.OPEN_FILTER_DRAWER_LINK_ICON_BUTTON;
-            this._openFilterButton.iconName = IconNames.FILTER_LIST;
-            this._openFilterButton.layoutData = new AnchorLayoutData( NaN, NaN, 4, NaN, NaN, 0 );
-        }
-        return this._openFilterButton;
     }
     get titleTextElement()
     {
@@ -54,58 +36,29 @@ export default class AvaAppBar extends AppBar
         }
         return this._titleTextElement;
     }
-    get openTrailersFilterButton()
+    get openSortButton()
     {
-        if( !this._openTrailersFilterButton )
+        if( !this._openSortButton )
         {
-            this._openTrailersFilterButton = new LinkIconButton();
-            this._openTrailersFilterButton.href = Paths.TRAILERS_GENRER;
-            this._openTrailersFilterButton.ariaLabel = Strings.OPEN_FILTER_MENU;
-            this._openTrailersFilterButton.uid = UIDS.OPEN_TRAILERS_FILTER_BUTTON;
-            this._openTrailersFilterButton.iconName = IconNames.FILTER_LIST;
-            this._openTrailersFilterButton.layoutData = new AnchorLayoutData( NaN, NaN, 4, NaN, NaN, 0 );
+            this._openSortButton = new IconButton();
+            this._openSortButton.ariaLabel = Strings.OPEN_SORT_MENU;
+            this._openSortButton.uid = UIDS.OPEN_SORT_BUTTON;
+            this._openSortButton.iconName = IconNames.SORT;
+            this._openSortButton.layoutData = new AnchorLayoutData( NaN, NaN, 52, NaN, NaN, 0 );
         }
-        return this._openTrailersFilterButton;
+        return this._openSortButton;
     }
-    get openMoviesFilterButton()
+    get openFilterButton()
     {
-        if( !this._openMoviesFilterButton )
+        if( !this._openFilterButton )
         {
-            this._openMoviesFilterButton = new LinkIconButton();
-            this._openMoviesFilterButton.isVisible = false;
-            this._openMoviesFilterButton.href = Paths.MOVIES_GENRER;
-            this._openMoviesFilterButton.ariaLabel = Strings.OPEN_FILTER_MENU;
-            this._openMoviesFilterButton.uid = UIDS.OPEN_MOVIES_FILTER_BUTTON;
-            this._openMoviesFilterButton.iconName = IconNames.FILTER_LIST;
-            this._openMoviesFilterButton.layoutData = new AnchorLayoutData( NaN, NaN, 4, NaN, NaN, 0 );
+            this._openFilterButton = new IconButton();
+            this._openFilterButton.ariaLabel = Strings.OPEN_FILTER_MENU;
+            this._openFilterButton.uid = UIDS.OPEN_FILTER_BUTTON;
+            this._openFilterButton.iconName = IconNames.FILTER_LIST;
+            this._openFilterButton.layoutData = new AnchorLayoutData( NaN, NaN, 4, NaN, NaN, 0 );
         }
-        return this._openMoviesFilterButton;
-    }
-    get openActorsFilterButton()
-    {
-        if( !this._openActorsFilterButton )
-        {
-            this._openActorsFilterButton = new LinkIconButton();
-            this._openActorsFilterButton.isVisible = false;
-            this._openActorsFilterButton.href = Paths.ACTORS_COUNTRIES;
-            this._openActorsFilterButton.ariaLabel = Strings.OPEN_FILTER_MENU;
-            this._openActorsFilterButton.uid = UIDS.OPEN_ACTORS_FILTER_BUTTON;
-            this._openActorsFilterButton.iconName = IconNames.FILTER_LIST;
-            this._openActorsFilterButton.layoutData = new AnchorLayoutData( NaN, NaN, 4, NaN, NaN, 0 );
-        }
-        return this._openActorsFilterButton;
-    }
-    get openSortMenuIconButton()
-    {
-        if( !this._openSortMenuIconButton )
-        {
-            this._openSortMenuIconButton = new IconButton();
-            this._openSortMenuIconButton.ariaLabel = Strings.OPEN_SORT_MENU;
-            this._openSortMenuIconButton.uid = UIDS.OPEN_SORT_MENU_ICON_BUTTON;
-            this._openSortMenuIconButton.iconName = IconNames.SORT;
-            this._openSortMenuIconButton.layoutData = new AnchorLayoutData( NaN, NaN, 52, NaN, NaN, 0 );
-        }
-        return this._openSortMenuIconButton;
+        return this._openFilterButton;
     }
     set title( value )
     {
