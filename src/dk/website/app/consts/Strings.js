5d2b0fda2a13f327189844bb48624375c713422a
Martin Rossil <martinrossil@gmail.com>
2018-09-28 10:01:54 +0200

    Added more Strings Constants.

--- src/dk/website/app/consts/Strings.js
+++ src/dk/website/app/consts/Strings.js
@@ -1,9 +1,12 @@
 export default class Strings
 {
-    static get OPEN_FILTER_MENU(){return "Åbn Filter Menu";};
-    static get CLOSE_FILTER_MENU(){return "Luk Filter Menu";};
-    static get CLOSE(){return "Luk";};
-    static get OPEN_SORT_MENU(){return "Åbn Sorterings Menu";};
-    static get GENRES(){return "Genrer";};
-    static get COUNTRIES(){return "Lande";};
+    static get OPEN_FILTER_MENU(){  return "Åbn Filter Menu";};
+    static get CLOSE_FILTER_MENU(){ return "Luk Filter Menu";};
+    static get CLOSE(){             return "Luk";};
+    static get OPEN_SORT_MENU(){    return "Åbn Sorterings Menu";};
+    static get GENRES(){            return "Genrer";};
+    static get COUNTRIES(){         return "Lande";};
+    static get TRAILERS(){          return "Trailers";};
+    static get MOVIES(){            return "Film";};
+    static get ACTORS(){            return "Skuespillere";};
 }
