223cea515d9ee6b86ba20d3bdabe35b796a1fb3f
Martin Rossil <martinrossil@gmail.com>
2018-09-28 20:48:52 +0200

    Added direct data handling for Item Render clicks.

--- src/dk/website/app/behavior/TrailerPlaybackBehavior.js
+++ src/dk/website/app/behavior/TrailerPlaybackBehavior.js
@@ -3,6 +3,8 @@ import Paths from "../consts/Paths.js";
 import UIDS from "../consts/UIDS.js";
 import Properties from "../consts/Properties.js";
 import ClickTargetUtil from "../utils/ClickTargetUtil.js";
+import TrailerItemRenderer from "../itemrenderers/TrailerItemRenderer.js"
+import MovieInfoTrailerItemRenderer from "../itemrenderers/MovieInfoTrailerItemRenderer.js";
 export default class TrailerPlaybackBehavior extends BaseBehavior
 {
     constructor()
@@ -11,14 +13,16 @@ export default class TrailerPlaybackBehavior extends BaseBehavior
     }
     clicked( e )
     {
-        //super.clicked( e );
-        
         let itemRenderer = ClickTargetUtil.getItemRendererFromTarget( e.target );
-        console.log( "TrailerPlaybackBehavior", "clicked", itemRenderer );
+        if( itemRenderer instanceof TrailerItemRenderer || itemRenderer instanceof MovieInfoTrailerItemRenderer )
+        {
+            let data = itemRenderer.data;
+            this.setProperty( UIDS.TRAILER_DIALOG, Properties.IS_SHOWN, true );
+            this.setProperty( UIDS.TRAILER_DIALOG, Properties.DATA, data );
+        }
     }
     pathChanged( path )
     {
-        console.log( "TrailerPlaybackBehavior", "pathChanged", path );
         let pathArray = path.split( "/" );
             pathArray.shift();
         const len = pathArray.length;
