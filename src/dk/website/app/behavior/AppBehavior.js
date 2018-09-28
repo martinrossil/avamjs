d43516c3a5fe86ed809d68e3d351b9fc3744074f
Martin Rossil <martinrossil@gmail.com>
2018-09-28 20:48:19 +0200

    Added Movie and Actor Info Behavior.

--- src/dk/website/app/behavior/AppBehavior.js
+++ src/dk/website/app/behavior/AppBehavior.js
@@ -3,6 +3,8 @@ import FilterDrawerBehavior from "./FilterDrawerBehavior.js";
 import NavigationBehavior from "./NavigationBehavior.js";
 import ListDataBehavior from "./ListDataBehavior.js";
 import TrailerPlaybackBehavior from "./TrailerPlaybackBehavior.js";
+import MovieInfoBehavior from "./MovieInfoBehavior.js";
+import ActorInfoBehavior from "./ActorInfoBehavior.js";
 export default class AppBehavior
 {
     constructor()
@@ -12,5 +14,7 @@ export default class AppBehavior
         new NavigationBehavior();
         new ListDataBehavior();
         new TrailerPlaybackBehavior();
+        new MovieInfoBehavior();
+        new ActorInfoBehavior();
     }
 }
