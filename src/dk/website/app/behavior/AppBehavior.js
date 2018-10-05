dccd4c76aa45cd4b4b6c101da65ea5859d4c3bb0
Martin Rossil <martinrossil@gmail.com>
2018-10-05 12:44:33 +0200

    Removed ServiceWorker behavior and moved to Ava App.

--- src/dk/website/app/behavior/AppBehavior.js
+++ src/dk/website/app/behavior/AppBehavior.js
@@ -5,12 +5,10 @@ import ListDataBehavior from "./ListDataBehavior.js";
 import TrailerPlaybackBehavior from "./TrailerPlaybackBehavior.js";
 import MovieInfoBehavior from "./MovieInfoBehavior.js";
 import ActorInfoBehavior from "./ActorInfoBehavior.js";
-import ServiceWorkerBehavior from "./ServiceWorkerBehavior.js";
 export default class AppBehavior
 {
     constructor()
     {
-        new ServiceWorkerBehavior();
         new HistoryBehavior();
         new FilterDrawerBehavior();
         new NavigationBehavior();
